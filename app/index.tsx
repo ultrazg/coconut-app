35247e94306bab8abf11f1819748f6a6ca35c41d
ultra2g <742005790@qq.com>
2024-03-24 02:02:34 +0800

    feat: 增加了无用的功能

--- app/index.tsx
+++ app/index.tsx
@@ -2,22 +2,43 @@ import { Text, View, StyleSheet, Pressable, Button } from 'react-native'
 import { router } from 'expo-router'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { Image } from 'expo-image'
+import * as Haptics from 'expo-haptics'
+import { useEffect, useState } from 'react'
 
 const Index = () => {
+  const [count, setCount] = useState(0)
+
+  const onEgg = () => {
+    setCount(count + 1)
+  }
+
+  useEffect(() => {
+    if (count === 10) {
+      Haptics.notificationAsync(
+        Haptics.NotificationFeedbackType.Success
+      )
+    }
+  }, [count])
+
   return (
     <SafeAreaView>
       <View style={styles.container}>
         <Image
           style={{ width: '100%', height: 100 }}
           source={require('../assets/icon.png')}
-          contentFit="contain"
+          contentFit='contain'
         />
-        <Text style={styles.title}>coconut</Text>
+        <Pressable onPress={() => {
+          onEgg()
+        }}>
+          <Text style={styles.title}>{count < 10 ? 'coconut' : `tunococ`}</Text>
+        </Pressable>
+
         <View style={styles.options}>
           <Pressable style={styles.searchButton}>
             <Button
-              title="搜索"
-              color="white"
+              title='搜索'
+              color='white'
               onPress={() => {
                 router.push('/search')
               }}
@@ -25,7 +46,7 @@ const Index = () => {
           </Pressable>
 
           <Button
-            title="关于..."
+            title='关于...'
             onPress={() => {
               router.push('/about')
             }}
@@ -40,23 +61,23 @@ const styles = StyleSheet.create({
   container: {
     height: 500,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
   title: {
     fontSize: 75,
     fontWeight: 'bold',
-    color: '#636363',
+    color: '#636363'
   },
   options: {
     paddingTop: 60,
-    fontSize: 30,
+    fontSize: 30
   },
   searchButton: {
-    width: 200,
+    width: 250,
     backgroundColor: '#1890ff',
     borderRadius: 10,
-    marginBottom: 10,
-  },
+    marginBottom: 10
+  }
 })
 
 export default Index
